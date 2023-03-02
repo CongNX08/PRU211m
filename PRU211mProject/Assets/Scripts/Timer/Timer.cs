c75b7d0c59c865e46c69357fcb4662d492764836
congnxhe151350 <congnxhe151350@fpt.edu.vn>
2023-03-02 23:59:14 +0700

    create gun

--- PRU211mProject/Assets/Scripts/Timer/Timer.cs
+++ PRU211mProject/Assets/Scripts/Timer/Timer.cs
@@ -8,6 +8,7 @@ public class Timer : MonoBehaviour
     public bool isFinish;
     public float elapseTime;
     public float arlarmTime;
+    public float arlarmTime2;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +18,7 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        arlarmTime2 = Time.time;
         elapseTime += Time.deltaTime;
         if (elapseTime >= arlarmTime)
         {
