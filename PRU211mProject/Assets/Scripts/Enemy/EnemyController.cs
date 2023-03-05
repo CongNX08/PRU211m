f0f6a3f6c25ff62e627a4143b74206684a3fe646
congnxhe151350 <congnxhe151350@fpt.edu.vn>
2023-03-05 22:51:51 +0700

    create collide bullet and enemy

---
+++ PRU211mProject/Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    int heal = 2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Bullet1"))
+        {
+            heal--;
+
+            Destroy(collision.gameObject);
+            if (heal == 0)
+            {
+                Destroy(gameObject);
+                           
+            }
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
