0996d0479212ae449c9207097264584393068ef6
unknown <datt19112001@gmail.com>
2023-03-09 18:11:54 +0700

    fixx

--- PRU211mProject/Assets/Scripts/Enemy/SpawnEnemy.cs
+++ PRU211mProject/Assets/Scripts/Enemy/SpawnEnemy.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class SpawnEnemy : MonoBehaviour
 {
     // Start is called before the first frame update
+    List<GameObject> enemys;
     protected float timeSpawn = 0f;
     protected float timeDlay = 3f;
-    public GameObject WalkPrefab;
+    public GameObject Enermy;
     void Start()
     {
-
+        this.enemys = new List<GameObject>();
     }
 
     // Update is called once per frame
@@ -18,12 +19,24 @@ public class SpawnEnemy : MonoBehaviour
     {
         this.timeSpawn += Time.deltaTime;
         if (this.timeSpawn < this.timeDlay) return;
-        else
+        this.timeSpawn = 0;
+        if (this.enemys.Count > 10) return;
+        int index = this.enemys.Count + 1;
+        GameObject minion = Instantiate(Enermy);
+        minion.transform.position = new Vector2(Random.Range(-60,60), Random.Range(-23,23));
+        this.enemys.Add(minion);
+        CheckEnemysDeath();
+    }
+    void CheckEnemysDeath()
+    {
+        GameObject enemysdie;
+        for (int i = 0; i < this.enemys.Count; i++)
         {
-            this.timeSpawn = 0;
-            GameObject minion = Instantiate(WalkPrefab);
-            minion.transform.position = transform.position;
+            enemysdie = this.enemys[i];
+            if (enemysdie == null)
+            {
+                this.enemys.RemoveAt(i);
+            }
         }
-
     }
 }
