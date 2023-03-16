069ba98b0d6fca1052af043d31e79fe2f0f3da1f
huyFPTthainguyen <15042001huy@gmail.com>
2023-03-16 23:33:41 +0700

    fix button, map and create gameover,exit

--- PRU211mProject/Assets/Scripts/UI/MenuEvent.cs
+++ PRU211mProject/Assets/Scripts/UI/MenuEvent.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System;
 
 public class MenuEvent : MonoBehaviour
 {
     public GameObject gameoverPanel;
+    public GameObject pausePanel;
+    public Button playPanel;
+    public Button exitPanel;
     public void Play(int index)
     {
         SceneManager.LoadScene(index);
@@ -21,5 +25,22 @@ public class MenuEvent : MonoBehaviour
     public void ShowGameoverPanel(bool isShow)
     {
         gameoverPanel.SetActive(isShow);
+    }
+
+    public void ReplayPanel()
+    {
+        Time.timeScale = 0;
+        pausePanel.SetActive(true);
+    }
+
+    public void PlayPanel()
+    {
+        Time.timeScale = 1;
+        pausePanel.SetActive(false);
+    }
+
+    public void ExitPanel(int index)
+    {
+        SceneManager.LoadScene(index);
     }
 }
